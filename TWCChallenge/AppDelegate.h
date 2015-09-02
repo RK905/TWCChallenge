684fcfd18f27a18be32d70287dc1614bfb566bca
RK905 <mr.rayenk@gmail.com>
2015-09-02 14:15:43 -0400

    Initial Commit

---
+++ TWCChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TWCChallenge
+//
+//  Created by Rayen Kamta on 9/2/15.
+//  Copyright (c) 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TWCChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TWCChallenge
+//
+//  Created by Rayen Kamta on 9/2/15.
+//  Copyright (c) 2015 Geeksdobyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
